e44e2f023b7871a7708c96072857b631641e9387
chiangwanyi <chiangwanyi@163.com>
2021-01-18 17:48:42 +0800

    WangEditorTest

--- mul-sharedb-test/src/components/WangEditorTest.vue
+++ mul-sharedb-test/src/components/WangEditorTest.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="wang-test">
         <h1>ShareDB 测试页面</h1>
+        <button @click="shutdown">中止</button>
         <div>
             <h2>建立连接</h2>
             <span>GroupId:</span><input type="text" v-model="groupId"/><span
@@ -11,23 +12,8 @@
             </button>
         </div>
         <hr>
-        <!--        <div>-->
-        <!--            <h2>操作</h2>-->
-        <!--            <span>获取第</span><input type="number" v-model="lineIndex"><span>行数据：</span>-->
-        <!--            <br>-->
-        <!--            <textarea cols="30" rows="10" v-model="lineText"></textarea><br>-->
-        <!--            <button @click="updateLine">提交修改</button>-->
-        <!--            <hr>-->
-        <!--            <button @click="insertNewLine(0, 'aaa')">创建新行</button>-->
-        <!--            <button @click="deleteLine(0)">删除行</button>-->
-        <!--        </div>-->
-        <!--        <hr>-->
-        <!--        <div>-->
-        <!--            <h2>原始数据</h2>-->
-        <!--            <p v-for="(item, index) in originData" :key="index">{{item.text}}</p>-->
-        <!--        </div>-->
         <div>
-                <div id="container"></div>
+            <div id="container"></div>
         </div>
     </div>
 </template>
@@ -39,6 +25,8 @@
 
     const sharedb = require("sharedb/lib/client");
     const json1 = require("ot-json1");
+
+    // const emptyDomReg = new RegExp("<[a-z|A-Z]+[0-9]?><br></[a-z|A-Z]+[0-9]?>");
 
     sharedb.types.register(json1.type);
 
@@ -54,21 +42,21 @@
                 groupId: "2",
                 userId: "0",
                 ready: false,
-                lineIndex: undefined,
-                lineText: '',
-                originData: [],
-                originJson: [],
                 editor: null,
+                freeze: false,
             };
         },
         methods: {
+            shutdown() {
+                window.location.href = 'http://www.baidu.com';
+            },
             connectServer() {
                 if (this.groupId === "" || this.userId === "") {
                     window.alert("请检查");
                     return;
                 }
                 this.socket = new ReconnectingWebSocket(
-                    "ws://localhost:9000?groupId=" +
+                    "ws://192.168.31.123:9000?groupId=" +
                     this.groupId +
                     "&" +
                     "userId=" +
@@ -81,8 +69,6 @@
                     if (err) throw err;
                     let docData = this.doc.data;
                     let docType = this.doc.type;
-                    console.log("文档data：", docData);
-                    console.log("文档type：", docType);
                     if (docData === undefined || docType === undefined) {
                         console.warn(`文档：${this.groupId}未创建，正在创建，然后重新订阅`);
                         this.doc.create({
@@ -96,51 +82,93 @@
                         this.doc.destroy();
                     } else {
                         this.ready = true;
-                        this.doc.subscribe(this.updatedOriginData);
-                        setTimeout(() => {
-                            this.lineIndex = 1;
-                        }, 500);
+                        this.doc.subscribe(this.updateLocalData(true));
                         this.doc.on('op', (op, source) => {
-                            console.log("接收到操作：" + JSON.stringify(op));
+                            console.log(`接收到操作\top:${JSON.stringify(op)}\t source:${source}`)
                             if (source) {
                                 console.log("操作来自本地");
                             } else {
                                 console.log("操作来自服务器，接收");
-                                this.updatedOriginData();
+                                this.updateLocalData(false);
                             }
                         });
                     }
                 });
             },
-            h() {
-                this.doc.submitOp(['data', 0, ['text', {es: [4, {d: 2}]}]], {source: this.userId});
-            },
-            queryLineData(index) {
-                for (let i = 0; i < this.originData.length; i++) {
-                    if (i === index - 1) {
-                        return this.strToUtf16(this.originData[i].text);
-                    }
-                }
-            },
-            updatedOriginData() {
-                this.originData = this.doc.data.data;
+            updateLocalData(init) {
+                this.freeze = true;
                 let html = "";
-                this.originData.forEach(el => {
+                // 获取最新数据
+                this.doc.data.data.forEach(el => {
                     html += el.text;
-                })
-                if (this.editor === null) {
-                    document.querySelector("div#container").innerHTML = html;
-                    this.editor = new Editor('#container');
-                    this.editor.config.onchange = (newHtml) => {
-                        console.log("本地修改");
-                        console.log(newHtml);
-                        // this.originJson = this.editor.txt.getJSON();
-                    }
+                });
+                console.log(html);
+                if (init) {
+                    console.log("初始化数据");
+                    document.getElementById("container").innerHTML = html;
                     this.editor.create();
                 } else {
-                    this.editor.txt.html(html);
+                    // 创建最新数据的DOM元素
+                    let element = document.createElement("div");
+                    element.innerHTML = this.strToUtf16(html);
+                    // 当前的数据DOM
+                    let container = document.getElementById(this.editor.textElemId);
+                    for (let i = 0; i < element.children.length; i++) {
+                        let node = element.children[i];
+                        console.log(`第${i}行：${node.outerHTML}`);
+                        if (i < container.children.length) {
+                            if (container.children[i].outerHTML !== node.outerHTML) {
+                                console.log("【修改】");
+                                container.children[i].outerHTML = node.outerHTML;
+                            } else {
+                                console.log("【不变】");
+                            }
+                        } else {
+                            console.log("【添加】");
+                            container.appendChild(node);
+                        }
+                    }
+                    if (container.children.length > element.children.length) {
+                        console.log("【移除】");
+                        container.removeChild(container.children[element.children.length]);
+                    }
+                    console.log("修改完毕");
+                    console.log(document.getElementById(this.editor.textElemId).innerHTML);
                 }
-                // this.originJson = this.editor.txt.getJSON();
+                this.freeze = false;
+            },
+            saveData() {
+                if (this.freeze) {
+                    console.log("本地修改监听冻结中...");
+                    return;
+                }
+                console.log("监听到本地修改");
+                let childNodes = document.getElementById(this.editor.textElemId).children;
+                let j = 0;
+                for (let i = 0; i < this.doc.data.data.length; i++) {
+                    let oldText = this.doc.data.data[i].text;
+                    let newText = j < childNodes.length ? childNodes[j++].outerHTML : null;
+                    // 先转码
+                    if (newText !== null) {
+                        newText = this.utf16ToStr(newText);
+                    }
+                    if (newText === null) {
+                        console.log(`第${i}行：【移除行】${oldText} -> ${newText}`);
+                        this.deleteLine(i);
+                    } else if (oldText === newText) {
+                        console.log(`第${i}行：【无变化】`);
+                    } else {
+                        console.log(`第${i}行：【修改行】${oldText} -> ${newText}`);
+                        this.updateLine(oldText, newText, i);
+                    }
+                }
+                while (j < childNodes.length) {
+                    let newText = this.utf16ToStr(childNodes[j].outerHTML);
+                    console.log(`第${j}行：【增加行】null -> ${newText}`);
+                    this.insertLine(j, newText);
+                    j++;
+                }
+                console.log("===============");
             },
             utf16ToStr(str) {
                 let regExp = /[\ud800-\udbff][\udc00-\udfff]/g; // 检测utf16字符正则
@@ -162,42 +190,29 @@
                     return String.fromCodePoint(parseInt("0x" + el.replace("&#", "").replace(";", ""), 16));
                 })
             },
-            updateLine() {
-                let index = this.lineIndex;
-                let newVal = this.utf16ToStr(this.lineText);
-                let oldVal = this.originData[index - 1].text;
-                console.log(`${oldVal} -> ${newVal}`);
+            updateLine(oldVal, newVal, index) {
                 let opt = ot.opt(oldVal, newVal);
-                console.log(opt);
                 if (opt.d === undefined) {
-                    this.doc.submitOp(['data', index - 1, ['text', {es: [opt.r, opt.i]}]]);
+                    this.doc.submitOp(['data', index, ['text', {es: [opt.r, opt.i]}]]);
                 } else {
-                    this.doc.submitOp(['data', index - 1, ['text', {es: [opt.r, {d: opt.d.len}, opt.i]}]]);
+                    this.doc.submitOp(['data', index, ['text', {es: [opt.r, {d: opt.d.len}, opt.i]}]]);
                 }
-                this.updatedOriginData();
             },
-            insertNewLine(index, text) {
+            insertLine(index, text) {
                 this.doc.submitOp(['data', index, {i: {text: text}}]);
-                this.updatedOriginData();
             },
             deleteLine(index) {
                 this.doc.submitOp(['data', index, {r: 0}]);
-                this.updatedOriginData();
             },
-            render() {
-                // this.updatedOriginData();
-            }
         },
-        watch: {
-            lineIndex(val) {
-                if (!this.ready) {
-                    return;
-                }
-                console.log(`读取第${val}行数据`);
-                let lineData = this.queryLineData(val);
-                this.lineText = lineData === undefined ? "=====无数据=====" : lineData;
-            }
-        },
+        mounted() {
+            this.editor = new Editor('#container');
+            this.editor.config.onchange = this.saveData;
+            // this.editor.create();
+            // setInterval(() => {
+            //     console.log(this.editor.selection)
+            // }, 2000);
+        }
     }
 </script>
 
