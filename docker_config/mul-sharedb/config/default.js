c8b89ff701fecbe328ff0155d620ea8c8cd8c692
chiangwanyi <chiangwanyi@163.com>
2021-04-20 21:23:12 +0800

    完成

---
+++ docker_config/mul-sharedb/config/default.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // HTTP 端口
+    port: 9003,
+
+    // 数据库
+    mongodb_addr: "mongodb://172.40.1.4:27017/mul-sharedb",
+
+    // 后台通信key
+    key: "73FB6FB50711641F153F1F09D04B6F8A",
+
+    // 后台入口
+    backend: "http://172.40.1.30:12000/api"
+};
